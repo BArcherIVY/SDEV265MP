b9fe181afbbf463afb5c4a156feb732cbe126a8a
Richie <Richie@DESKTOP-MAOEM3F>
2026-04-26 23:17:47 -0400

    Changed Main to enable testing of the begginings of the annex class. The annex class currently prints the file list in the console log will setup with JavaFX soon for UI to begin working. Just trying to make sure we have something to display lol.

--- MediaPlayer/src/fileControl/Annex.java
+++ MediaPlayer/src/fileControl/Annex.java
@@ -1,5 +1,33 @@
 package fileControl;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
+
 
 public class Annex {
-
+	//Pass the folder name to annex (Arg Folder). This should be called on startup in main.
+	//Returns a 1 for success, and a negative 1 for error. Errors will re-prompt user to select folder.
+	public static int RunAnnex(String Folder) { 
+		Path targetFolder = Paths.get(Folder);
+		
+		Map<String, String> fileMap = new HashMap<>(); //God, I love hashmaps! <Songname, path>
+		
+		try (Stream<Path> stream = Files.walk(targetFolder)) {
+            stream.filter(Files::isRegularFile).forEach(
+        		path -> { //This looks scary, I promise you it's not. Essentially filter.forEach.Getmethedamnpath
+        			fileMap.put(path.getFileName().toString(), path.toAbsolutePath().toString());
+        		}
+        	);
+            fileMap.forEach((name, path) -> System.out.println(name + " -> " + path));
+            return 1;
+			
+		} catch (IOException e) {
+			System.err.println("Error! " + targetFolder.toString() + " can't be found. How did the user fucking manage that?");
+			return -1;
+		}
+	}
 }
--- MediaPlayer/src/program/Main.java
+++ MediaPlayer/src/program/Main.java
@@ -1,7 +1,26 @@
 package program;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("Hello, World!");
+		JFileChooser chooser = new JFileChooser(); //file prompt
+		chooser.setDialogTitle("Select a Folder");
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int result = chooser.showOpenDialog(null);
+		
+		if (result == JFileChooser.APPROVE_OPTION) {
+			
+            File selectedFile = chooser.getSelectedFile();
+            System.out.println("Selected path: " + selectedFile.getAbsolutePath());
+            fileControl.Annex.RunAnnex(selectedFile.getAbsolutePath());
+        } else {
+        	
+            System.out.println("No selection made.");
+            
+        }
+        
     }
 }
